ded5ec72da3d5d19a7d98a9c9db74a1f2c195344
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-07-31 02:01:31 -0600

    feat: Add TabsLayout component for tab navigation

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,119 @@
+import { Tabs } from 'expo-router'
+import { Image, Text, View } from 'react-native'
+import { Icons } from '../../constants'
+
+const TabIcon = ( { icon, color, name, focused } ) => {
+  return (
+    <View className='flex items-center justify-center flex-col gap-0.5'>
+      <Image
+        source={icon}
+        resizeMode='contain'
+        tintColor={color}
+        className='w-6 h-6'
+      />
+      <Text style={{ color: color }} className={`${focused ? 'font-GilroySemibold' : 'font-GilroyRegular'} text-xs`}>
+        {name}
+      </Text>
+    </View>
+  )
+}
+
+const TabsLayout = () => {
+  return (
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: '#367BFF',
+          tabBarInactiveTintColor: '#999999',
+          tabBarStyle: {
+            backgroundColor: '#FFFFFF',
+          }
+        }}
+      >
+
+        <Tabs.Screen
+          name='home'
+          options={{
+            headerShown: false,
+            title: 'Home',
+            tabBarIcon: ( { color, focused } ) => (
+              <TabIcon
+                icon={Icons.Home}
+                color={color}
+                name='Home'
+                focused={focused}
+              />
+            )
+          }}
+        />
+
+        <Tabs.Screen
+          name='maps'
+          options={{
+            headerShown: false,
+            title: 'Maps',
+            tabBarIcon: ( { color, focused } ) => (
+              <TabIcon
+                icon={Icons.Map}
+                color={color}
+                name='Maps'
+                focused={focused}
+              />
+            )
+          }}
+        />
+
+        <Tabs.Screen
+          name='membership'
+          options={{
+            headerShown: false,
+            title: 'Membership',
+            tabBarIcon: ( { color, focused } ) => (
+              <TabIcon
+                icon={Icons.Diamond}
+                color={color}
+                name='Membership'
+                focused={focused}
+              />
+            )
+          }}
+        />
+
+        <Tabs.Screen
+          name='profile'
+          options={{
+            headerShown: false,
+            title: 'Profile',
+            tabBarIcon: ( { color, focused } ) => (
+              <TabIcon
+                icon={Icons.Person}
+                color={color}
+                name='Profile'
+                focused={focused}
+              />
+            )
+          }}
+        />
+
+        <Tabs.Screen
+          name='settings'
+          options={{
+            headerShown: false,
+            title: 'Settings',
+            tabBarIcon: ( { color, focused } ) => (
+              <TabIcon
+                icon={Icons.Settings}
+                color={color}
+                name='Settings'
+                focused={focused}
+              />
+            )
+          }}
+        />
+      </Tabs>
+    </>
+  )
+}
+
+export default TabsLayout
