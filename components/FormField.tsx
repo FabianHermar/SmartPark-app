44a1d56a748dbef08ebc915b22bfd8f399c852bb
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-07-31 02:03:04 -0600

    feat: Add FormField component

---
+++ components/FormField.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const FormField = () => {
+  return (
+    <View>
+      <Text>FormField</Text>
+    </View>
+  )
+}
+
+export default FormField
