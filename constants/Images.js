c5f526b550c568ef12844fd812345eeadccbbc7e
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-07-31 02:03:26 -0600

    feat: Add images constants

---
+++ constants/Images.js
@@ -0,0 +1,33 @@
+import Basement from "@/assets/images/basement.jpg";
+import BlueGradient6 from "@/assets/images/blue-gradient-6.png";
+import BlueGradient8 from "@/assets/images/blue-gradient-8.png";
+import BlueGradient from "@/assets/images/blue-gradient.png";
+import CarIndex from "@/assets/images/car-index.png";
+import CarLot from "@/assets/images/CarLot.png";
+import Floor1 from "@/assets/images/floor1.jpg";
+import Floor2 from "@/assets/images/floor2.jpg";
+import GradientIndex from "@/assets/images/gradient-index.png";
+import Grainy from "@/assets/images/grainy.png";
+import IndexImage from "@/assets/images/index-image.png";
+import Logo from "@/assets/images/logo.png";
+import Membership from "@/assets/images/membership.jpg";
+import ProfileHeader from "@/assets/images/profile-header.jpg";
+import SmartParkLogo from "@/assets/images/SmartPark-logo.png";
+
+export default {
+  Basement,
+  BlueGradient,
+  BlueGradient6,
+  BlueGradient8,
+  CarIndex,
+  CarLot,
+  Floor1,
+  Floor2,
+  GradientIndex,
+  Grainy,
+  IndexImage,
+  Logo,
+  Membership,
+  ProfileHeader,
+  SmartParkLogo,
+}
