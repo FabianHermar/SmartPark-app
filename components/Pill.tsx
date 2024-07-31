806fef86a7d2786c4cce17a960933a690958a006
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-07-31 02:03:13 -0600

    feat: Add Pill component

---
+++ components/Pill.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Pill = () => {
+  return (
+    <View>
+      <Text>Pill</Text>
+    </View>
+  )
+}
+
+export default Pill
