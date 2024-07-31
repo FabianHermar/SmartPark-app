d3834cca54504f2ae1055217f352b35d0d94b4c5
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-07-31 02:00:28 -0600

    chore: Update RootLayout component to use Gilroy font family and hide splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,31 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+SplashScreen.preventAutoHideAsync()
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+export default function RootLayout () {
+  const [fontsLoaded, error] = useFonts( {
+    GilroyRegular: require( "@/assets/fonts/GilroyRegular.ttf" ),
+    GilroyMedium: require( "@/assets/fonts/GilroyMedium.ttf" ),
+    GilroySemibold: require( "@/assets/fonts/GilroySemibold.ttf" ),
+    GilroyBold: require( "@/assets/fonts/GilroyBold.ttf" ),
   });
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+    if ( error ) throw error
 
-  if (!loaded) {
-    return null;
-  }
+    if ( fontsLoaded ) SplashScreen.hideAsync()
+  }, [fontsLoaded, error] )
+
+  if ( !fontsLoaded && !error ) return null
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
-  );
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{ headerShown: false }}
+      />
+    </Stack>
+  )
 }
