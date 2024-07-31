bf2994317973c6bc38e25a553687673aa5e82439
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-07-31 02:03:22 -0600

    feat: Add constants for Icons

---
+++ constants/Icons.ts
@@ -0,0 +1,55 @@
+import Accessible from '@/assets/icons/accessible.png';
+import Bell from '@/assets/icons/bell.png';
+import Check from '@/assets/icons/check.png';
+import Crown from '@/assets/icons/crown.png';
+import Diamond from '@/assets/icons/diamond.png';
+import EyeOff from '@/assets/icons/eye-off.png';
+import Eye from '@/assets/icons/eye.png';
+import Headset from '@/assets/icons/headset.png';
+import Home from '@/assets/icons/home.png';
+import Info from '@/assets/icons/info.png';
+import Left from '@/assets/icons/left.png';
+import LifeBuoy from '@/assets/icons/life-buoy.png';
+import Map from '@/assets/icons/map.png';
+import Padlock from '@/assets/icons/padlock.png';
+import Parking from '@/assets/icons/parking.png';
+import Pencil from '@/assets/icons/pencil.png';
+import Person from '@/assets/icons/person.png';
+import Pin from '@/assets/icons/pin.png';
+import Right from '@/assets/icons/right.png';
+import Search from '@/assets/icons/search.png';
+import Settings from '@/assets/icons/settings.png';
+import Shield from '@/assets/icons/shield.png';
+import Sparkles from '@/assets/icons/sparkles.png';
+import User from '@/assets/icons/user.png';
+import Valet from '@/assets/icons/valet.png';
+import Verified from '@/assets/icons/verified.png';
+
+export default {
+  Accessible,
+  Bell,
+  Check,
+  Crown,
+  Diamond,
+  Eye,
+  EyeOff,
+  Headset,
+  Home,
+  Info,
+  Left,
+  LifeBuoy,
+  Map,
+  Padlock,
+  Parking,
+  Pencil,
+  Person,
+  Pin,
+  Right,
+  Search,
+  Settings,
+  Shield,
+  Sparkles,
+  User,
+  Valet,
+  Verified
+}
