e4f01b8bce134dd0be5d4a5eb659246524886271
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-01 00:08:25 -0600

    feat: Add ProfileSetup component

---
+++ app/(auth)/profile-setup.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const ProfileSetup = () => {
+  return (
+    <View>
+      <Text>ProfileSetup</Text>
+    </View>
+  )
+}
+
+export default ProfileSetup
