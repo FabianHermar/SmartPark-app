2ef2181a1064ab04c2b13eabc147b5948d6d0d46
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-01 00:09:21 -0600

    feat: Add Pill component

---
+++ components/Pill.jsx
@@ -0,0 +1,23 @@
+import { Icons } from '@/constants'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
+
+const Pill = ( { title, handlePress, containerStyles, textStyles, isLoading } ) => {
+  return (
+    <TouchableOpacity
+      onPress={handlePress}
+      activeOpacity={0.7}
+      className={`bg-black/10 backdrop-blur px-4 py-2 w-20 justify-center rounded-full items-center ${containerStyles} ${isLoading ? 'opacity-50' : ''}`}
+      disabled={isLoading}
+    >
+      <View className='flex flex-row gap-0.5 items-center justify-center'>
+        <Image
+          source={Icons.Left}
+          className='w-4 h-4 text-secondary'
+        />
+        <Text className={`text-secondary font-GilroySemibold text-xs ${textStyles}`}>{title}</Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+export default Pill
