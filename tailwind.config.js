0c58ab71490aa85b47d92ea7f78eaf067199c668
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-02 00:45:14 -0600

    feat: Update border color and add pill color in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,8 @@ module.exports = {
       colors: {
         primary: "#367BFF",
         secondary: "#666666",
-        border: "#CECECE"
+				border: "#CECECE",
+				pill: '#3C3F46'
       },
       backgroundImage: {
         'buttonGradient': "linear-gradient(90deg, #0F1B27 0%, #0B1521 35%, #050713 85%)",
