aca4e74667794eeae75f94cab12ec1d1f1e1dedb
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-02 00:44:44 -0600

    chore: Refactor ProfileSetup component layout and styles

--- app/(auth)/profile-setup.jsx
+++ app/(auth)/profile-setup.jsx
@@ -1,10 +1,40 @@
-import { Text, View } from 'react-native'
+import DefaultButton from '@/components/DefaultButton'
+import { BlueIcons, Images } from '@/constants'
+import { router } from 'expo-router'
+import React from 'react'
+import { Image, ScrollView, Text, View } from 'react-native'
+import ConfettiCannon from 'react-native-confetti-cannon'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const ProfileSetup = () => {
+
   return (
-    <View>
-      <Text>ProfileSetup</Text>
-    </View>
+		<SafeAreaView className='bg-white h-full'>
+			<ScrollView>
+				<ConfettiCannon count={100} origin={{ x: 30, y: 0 }} autoStart={true} />
+				<Image
+					source={Images.BlueGradient4}
+					className='w-full h-full absolute -top-56 opacity-80 rotate-180 left-0 -z-20'
+				/>
+				<View className='w-full justify-start px-4 py-28 h-full'>
+					<View className='w-full h-max mb-6 justify-start'>
+						<View className='flex justify-start items-center gap-1 flex-row'>
+							<Image source={BlueIcons.Check} className='w-6 h-6' />
+							<Text className='text-sm font-GilroyMedium'>Profile set up!</Text>
+						</View>
+						<Text className='text-3xl font-GilroySemibold'>Congrats John!</Text>
+						<Text className='text-3xl font-GilroySemibold'>You’re set to start</Text>
+						<Text className='text-sm font-GilroyRegular'>Thank you for choosing us.</Text>
+					</View>
+
+					<DefaultButton
+						title='Back to Sign In'
+						handlePress={() => router.replace( 'sign-in' )}
+						containerStyles='mt-[415px]'
+					/>
+				</View>
+			</ScrollView>
+		</SafeAreaView>
   )
 }
 
