ea1746568b3cc37d76cddda89ba19016d4c2fe41
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-03 22:47:59 -0600

    feat: Add category checkboxes and handle selection

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,31 @@
+import CategoryCard from "@/components/CategoryCard";
 import { Icons, Images } from "@/constants";
 import { Image } from 'expo-image';
 import { router } from "expo-router";
+import { useState } from "react";
 import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function UserIndex () {
+	const [ selectedCheckbox, setSelectedCheckbox ] = useState( null )
+
+	const checkboxes = [
+		{ id: 1, text: 'Car', image: Icons.Car },
+		{ id: 2, text: 'Bike', image: Icons.Motorcycle },
+		{ id: 3, text: 'Bicycle', image: Icons.Bicycle },
+		{ id: 4, text: 'Truck', image: Icons.Truck },
+	];
+
+	const handlePress = ( id ) => {
+		if ( selectedCheckbox === id ) {
+			setSelectedCheckbox( null )
+		} else {
+			setSelectedCheckbox( id )
+		}
+	};
+
 	return (
-		<SafeAreaView className='bg-white h-full'>
+		<SafeAreaView className='bg-white h-full' edges={{ top: 'maximum', left: 'maximum', right: 'maximum' }}>
 			<ScrollView contentContainerStyle={{ height: '100%' }}>
 				<View className='bg-black w-full h-[300px]'>
 					<View className='py-8 flex flex-row justify-center items-center gap-x-32'>
@@ -55,53 +74,51 @@ export default function UserIndex () {
 				<View className='px-4 mt-4'>
 					<Text className='text-2xl font-GilroySemibold'>Category</Text>
 					<View>
-						<View className='flex flex-row gap-x-4 mt-4'>
-							<TouchableOpacity
-								onPress={() => router.push( 'category' )}
-								activeOpacity={0.7}
-								className='bg-[#EFEFEF] w-[74px] h-24 shadow-sm rounded-lg flex items-center justify-center'
-							>
-								<Image
-									source={Icons.Car}
-									className='w-10 h-10'
+						<View className='flex flex-row justify-center items-center'>
+							{checkboxes.map( ( checkbox ) => (
+								<CategoryCard
+									key={checkbox.id}
+									text={checkbox.text}
+									imageSource={checkbox.image}
+									additionalContent={null}
+									checked={selectedCheckbox === checkbox.id}
+									onPress={() => handlePress( checkbox.id )}
 								/>
-								<Text className='font-GilroySemibold text-black text-lg mt-2'>Car</Text>
-							</TouchableOpacity>
-							<TouchableOpacity
-								onPress={() => router.push( 'category' )}
-								activeOpacity={0.7}
-								className='bg-[#EFEFEF] w-[74px] h-24 shadow-sm rounded-lg flex items-center justify-center'
-							>
-								<Image
-									source={Icons.Motorcycle}
-									className='w-10 h-10'
-								/>
-								<Text className='font-GilroySemibold text-black text-lg mt-2'>Bike</Text>
-							</TouchableOpacity>
-
-							<TouchableOpacity
-								onPress={() => router.push( 'category' )}
-								activeOpacity={0.7}
-								className='bg-[#EFEFEF] w-[74px] h-24 shadow-sm rounded-lg flex items-center justify-center'
-							>
-								<Image
-									source={Icons.Bicycle}
-									className='w-10 h-10'
-								/>
-								<Text className='font-GilroySemibold text-black text-lg mt-2'>Bicycle</Text>
-							</TouchableOpacity>
-							<TouchableOpacity
-								onPress={() => router.push( 'category' )}
-								activeOpacity={0.7}
-								className='bg-[#EFEFEF] w-[74px] h-24 shadow-sm rounded-lg flex items-center justify-center'
-							>
-								<Image
-									source={Icons.Truck}
-									className='w-10 h-10'
-								/>
-								<Text className='font-GilroySemibold text-black text-lg mt-2'>Truck</Text>
-							</TouchableOpacity>
+							) )}
 						</View>
+					</View>
+				</View>
+				<View className='px-4 mt-10'>
+					<Text className='text-2xl font-GilroySemibold'>Most popular car parks</Text>
+					<View className='flex flex-row gap-x-4 mt-4'>
+						<TouchableOpacity
+							onPress={() => router.push( 'parking' )}
+							activeOpacity={0.7}
+							className='w-[150px] h-[150px] bg-[#EFEFEF] rounded-lg shadow-sm'
+						>
+							<Image
+								source={Images.Parking1}
+								className='w-full h-3/4 rounded-t-lg'
+							/>
+							<View className='px-4 pt-4'>
+								<Text className='font-GilroySemibold text-black text-lg'>Parking 1</Text>
+								<Text className='font-GilroyRegular text-secondary text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
+							</View>
+						</TouchableOpacity>
+						<TouchableOpacity
+							onPress={() => router.push( 'parking' )}
+							activeOpacity={0.7}
+							className='w-[150px] h-[150px] bg-[#EFEFEF] rounded-lg shadow-sm'
+						>
+							<Image
+								source={Images.Parking2}
+								className='w-full h-3/4 rounded-t-lg'
+							/>
+							<View className='px-4 pt-4'>
+								<Text className='font-GilroySemibold text-black text-lg'>Parking 2</Text>
+								<Text className='font-GilroyRegular text-secondary text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
+							</View>
+						</TouchableOpacity>
 					</View>
 				</View>
 			</ScrollView>
