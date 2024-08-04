b7c1bc1461d14b39a76a4aef833bb2913415fc82
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-03 22:48:35 -0600

    feat: Add CategoryCard component for displaying category checkboxes and handle selection

---
+++ components/CategoryCard.jsx
@@ -0,0 +1,53 @@
+import { Image } from 'expo-image';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+const Card = ( { text, imageSource, checked, onPress, disabled, additionalContent } ) => {
+
+	return (
+		<View className='m-2'>
+			<TouchableOpacity
+				style={[ styles.checkboxContainer, checked && styles.checked, disabled && styles.disabled ]}
+				className='bg-[#EFEFEF] w-[74px] h-24 shadow-sm rounded-lg flex items-center justify-center'
+				onPress={!disabled ? onPress : null}
+				activeOpacity={disabled ? 1 : 0.7}
+			>
+				<Image source={imageSource} className='w-10 h-10' />
+				<Text className='font-GilroySemibold text-black text-lg mt-2'>{text}</Text>
+			</TouchableOpacity>
+			{/* {checked && (
+				<View className='pt-4 invisible'>
+					{additionalContent}
+				</View>
+			)} */}
+		</View>
+	);
+};
+
+const styles = StyleSheet.create( {
+
+	checked: {
+		borderColor: '#007BFF',
+	},
+
+	disabledText: {
+		color: '#aaa',
+	},
+
+	disabled: {
+		backgroundColor: '#333333',
+	},
+
+	checkboxContainer: {
+		borderWidth: 2,
+		borderColor: 'transparent',
+		borderRadius: 5,
+
+	},
+	additionalContent: {
+		backgroundColor: '#f0f0f0',
+		padding: 10,
+		borderRadius: 5,
+	},
+} );
+
+export default Card;
