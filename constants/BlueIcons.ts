053312778828aabff251734be7fb794f7e282b10
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-05 12:19:55 -0600

    refactor: Update BlueIcons.ts to use SVG icons instead of PNG icons

--- constants/BlueIcons.ts
+++ constants/BlueIcons.ts
@@ -1,5 +1,21 @@
-import Check from '@/assets/blue-icons/check.png'
+import Check from '@/assets/blue-icons/check.svg'
+import Crown from '@/assets/blue-icons/crown.svg'
+import Customers from '@/assets/blue-icons/customers.svg'
+import Discount from '@/assets/blue-icons/discount.svg'
+import Parking from '@/assets/blue-icons/parking.svg'
+import Security from '@/assets/blue-icons/security.svg'
+import Stars from '@/assets/blue-icons/stars.svg'
+import Valet from '@/assets/blue-icons/valet.svg'
+import Verified from '@/assets/blue-icons/verified.svg'
 
 export default {
-	Check
+	Check,
+	Crown,
+	Customers,
+	Discount,
+	Parking,
+	Security,
+	Stars,
+	Valet,
+	Verified,
 }
