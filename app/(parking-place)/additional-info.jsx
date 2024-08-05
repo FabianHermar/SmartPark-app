a3b4a48b76e41aa0b0f5655cded0d361b3cd5cd7
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-05 00:45:35 -0600

    feat: Add AdditionalInfo component for displaying additional information about parking places

---
+++ app/(parking-place)/additional-info.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const AdditionalInfo = () => {
+	return (
+		<View>
+			<Text>AdditionalInfo</Text>
+		</View>
+	)
+}
+
+export default AdditionalInfo
