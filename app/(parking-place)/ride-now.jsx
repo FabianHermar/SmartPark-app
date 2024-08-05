964bc925b8b7b89a41d93e68e83523d6292a7335
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-05 00:45:53 -0600

    feat: Add RideNow component for initiating a ride

---
+++ app/(parking-place)/ride-now.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const RideNow = () => {
+	return (
+		<View>
+			<Text>RideNow</Text>
+		</View>
+	)
+}
+
+export default RideNow
