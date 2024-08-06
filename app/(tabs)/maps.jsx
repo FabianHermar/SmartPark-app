84702a1185e0d4d30f43fbea07690fc30f13f4d0
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-06 13:57:25 -0600

    refactor: Update maps.jsx to include informative text for the maps section

--- app/(tabs)/maps.jsx
+++ app/(tabs)/maps.jsx
@@ -3,7 +3,8 @@ import { StyleSheet, Text, View } from "react-native"
 export default function Maps () {
   return (
     <View style={styles.container}>
-      <Text>Maps</Text>
+			<Text className='font-GilroyMedium text-xl'>We're working on the maps section</Text>
+			<Text className='font-GilroyMedium text-xl'>Stay tuned!</Text>
     </View>
   )
 }
@@ -13,5 +14,6 @@ const styles = StyleSheet.create( {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+		backgroundColor: '#fff',
   },
 } )
