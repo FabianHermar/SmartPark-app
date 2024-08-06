b6007732ddb4b599dbcc7deb95a56394026fa863
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-06 13:58:19 -0600

    feat: Design of Membership section almost finished

--- app/(tabs)/membership.jsx
+++ app/(tabs)/membership.jsx
@@ -1,17 +1,48 @@
-import { StyleSheet, Text, View } from "react-native"
+import { Images } from '@/constants';
+import { BlurView } from 'expo-blur';
+import { Image } from 'expo-image';
+import { LinearGradient } from 'expo-linear-gradient';
+import { ImageBackground, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function Membership () {
-  return (
-    <View style={styles.container}>
-      <Text>Membership</Text>
-    </View>
-  )
+const Membership = () => {
+	return (
+		<SafeAreaView className='bg-black h-full' edges={{ top: 'maximum', left: 'maximum', right: 'maximum' }}>
+			<ScrollView>
+				<ImageBackground source={Images.Membership} style={styles.image}>
+					<View className='w-full justify-start h-full pt-10'>
+						<View className='items-center justify-center pt-10'>
+							<BlurView style={{ borderRadius: 20, overflow: 'hidden' }} className='bg-[#111111]/60 w-56 items-center justify-center' intensity={20}>
+								<Image
+									source={Images.SmartParkLogo}
+									contentFit='contain'
+									className='w-52 h-20'
+									tintColor='#FFF'
+								/>
+							</BlurView>
+						</View>
+					</View>
+				</ImageBackground>
+				<LinearGradient
+					colors={[ '#0F1B27', '#0B1521', '#050713' ]}
+					start={[ 0, 0 ]}
+					end={[ 1, 1 ]}
+					className='w-full h-48 items-start justify-center rounded-tl-[65px] mt-4 absolute bottom-0'
+				>
+					<Text className='text-white'>Hola</Text>
+				</LinearGradient>
+			</ScrollView>
+		</SafeAreaView>
+	)
 }
 
 const styles = StyleSheet.create( {
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-} )
+	image: {
+		flex: 1,
+		resizeMode: 'cover',
+		justifyContent: 'center',
+		height: 700,
+	}
+} );
+
+export default Membership
