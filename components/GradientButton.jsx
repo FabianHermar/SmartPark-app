1abc1d1cdf30a08488e6f92ab96ea57dd84b02c9
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-06 17:37:22 -0600

    refactor: Update GradientButton component to use expo-image for image handling and optimize images

--- components/GradientButton.jsx
+++ components/GradientButton.jsx
@@ -1,16 +1,30 @@
+import { Icons } from '@/constants'
+import { Image } from 'expo-image'
+import { LinearGradient } from 'expo-linear-gradient'
 import { Text, TouchableOpacity } from 'react-native'
 
-const DefaultButton = ( { title, handlePress, containerStyles, textStyles, isLoading } ) => {
+const GradientButton = ( { title, handlePress, containerStyles, textStyles } ) => {
   return (
     <TouchableOpacity
       onPress={handlePress}
-      activeOpacity={0.7}
-      className={`bg-black rounded-xl min-h-[70px] w-full justify-center items-center ${containerStyles} ${isLoading ? 'opacity-50' : ''}`}
-      disabled={isLoading}
-    >
-      <Text className={`text-white font-GilroySemibold text-lg ${textStyles}`}>{title}</Text>
+			activeOpacity={0.6}
+			className={`rounded-xl min-h-[70px] w-full justify-center items-center border-2 border-white/10 ${containerStyles}`}
+		>
+			<LinearGradient
+				colors={[ '#0F1B27', '#0B1521', '#050713' ]}
+				start={[ 0, 0 ]}
+				end={[ 1, 1 ]}
+				className='w-full min-h-[70px] items-center justify-center rounded-xl flex flex-row'
+			>
+				<Text className={`text-white font-GilroySemibold text-xl ${textStyles}`}>{title}</Text>
+				<Image
+					source={Icons.Right}
+					className='w-6 h-6'
+					tintColor='#FFF'
+				/>
+			</LinearGradient>
     </TouchableOpacity>
   )
 }
 
-export default DefaultButton
+export default GradientButton
