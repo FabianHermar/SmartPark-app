81a4af5e3c6095ad9cb399af6d23f5d89770855b
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-07 00:33:54 -0600

    refactor: Update Benefits.ts to include membership benefits data

---
+++ lib/Benefits.ts
@@ -0,0 +1,66 @@
+import { BlueIcons } from '@/constants'
+
+interface Benefit {
+	title: string
+	icon: string
+	benefits: string[]
+}
+
+export const Benefits: Benefit[] = [
+	{
+		title: 'Unlimited Parking',
+		icon: BlueIcons.Parking,
+		benefits: [
+
+		]
+	},
+	{
+		title: 'Reserved Spaces',
+		icon: BlueIcons.Verified,
+		benefits: [
+
+		]
+	},
+	{
+		title: 'Priority Access',
+		icon: BlueIcons.Crown,
+		benefits: [
+
+		]
+	},
+	{
+		title: 'Valet Service',
+		icon: BlueIcons.Valet,
+		benefits: [
+
+		]
+	},
+	{
+		title: 'Enhanced Security and Surveillance',
+		icon: BlueIcons.Security,
+		benefits: [
+
+		]
+	},
+	{
+		title: 'Discounts and Promotions',
+		icon: BlueIcons.Discount,
+		benefits: [
+
+		]
+	},
+	{
+		title: 'Access to Special Areas',
+		icon: BlueIcons.Stars,
+		benefits: [
+
+		]
+	},
+	{
+		title: 'Dedicated Customer Support',
+		icon: BlueIcons.Customers,
+		benefits: [
+
+		]
+	}
+]
