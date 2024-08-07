b16cc794a898ec46d341490142c8d7f50c95fce0
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-06 18:46:14 -0600

    feat: Add VehicleInfo component for vehicle information page

---
+++ app/(membership-pages)/vehicle-info.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const VehicleInfo = () => {
+	return (
+		<View>
+			<Text>VehicleInfo</Text>
+		</View>
+	)
+}
+
+export default VehicleInfo
