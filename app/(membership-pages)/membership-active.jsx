77e80b17e588b03706801ac36fee55a3ccf25737
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-06 18:45:53 -0600

    feat: Add MembershipActive component for active membership page

---
+++ app/(membership-pages)/membership-active.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const MembershipActive = () => {
+	return (
+		<View>
+			<Text>MembershipActive</Text>
+		</View>
+	)
+}
+
+export default MembershipActive
