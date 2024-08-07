d675d8c66a9086b8421573f5095345b091c389f2
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-06 18:45:45 -0600

    refactor: Add MembershipPagesLayout component for membership pages navigation

---
+++ app/(membership-pages)/_layout.tsx
@@ -0,0 +1,34 @@
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+
+const MembershipPagesLayout = () => {
+	return (
+		<>
+			<Stack>
+				<Stack.Screen
+					name='membership-active'
+					options={{ headerShown: false }}
+				/>
+				<Stack.Screen
+					name='membership-benefits'
+					options={{ headerShown: false }}
+				/>
+				<Stack.Screen
+					name='membership-info'
+					options={{ headerShown: false }}
+				/>
+				<Stack.Screen
+					name='payment-method'
+					options={{ headerShown: false }}
+				/>
+				<Stack.Screen
+					name='vehicle-info'
+					options={{ headerShown: false }}
+				/>
+			</Stack>
+			<StatusBar backgroundColor="white" style='dark' />
+		</>
+	)
+}
+
+export default MembershipPagesLayout
