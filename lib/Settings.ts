5daeb4a02e65c7b910253307044e583a0cd427ad
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-07 00:34:06 -0600

    refactor: Update Settings.ts to include settings data

---
+++ lib/Settings.ts
@@ -0,0 +1,33 @@
+import { Icons } from '@/constants'
+
+interface Setting {
+	title: string
+	icon: string
+}
+
+export const Settings: Setting[] = [
+	{
+		title: 'Account and Profile',
+		icon: Icons.Profile
+	},
+	{
+		title: 'Application preferences',
+		icon: Icons.Settings
+	},
+	{
+		title: 'Security and Privacy',
+		icon: Icons.Padlock
+	},
+	{
+		title: 'Accessibility',
+		icon: Icons.Accessible
+	},
+	{
+		title: 'Help and Support',
+		icon: Icons.LifeBuoy
+	},
+	{
+		title: 'Legal information',
+		icon: Icons.Info
+	}
+]
