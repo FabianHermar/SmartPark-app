76416769acd0b0923bade0f33b89a62ac609de7e
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-07 17:22:28 -0600

    refactor: Update Settings page layout and components with settings data

--- app/(tabs)/settings.jsx
+++ app/(tabs)/settings.jsx
@@ -1,5 +1,6 @@
 import DefaultButton from '@/components/DefaultButton'
 import { Icons, Images } from '@/constants'
+import settings from '@/lib/settings'
 import { Image } from 'expo-image'
 import { router } from 'expo-router'
 import { ScrollView, Text, View } from 'react-native'
@@ -18,90 +19,28 @@ const Settings = () => {
 						<Text className='text-5xl font-GilroyBold'>Settings</Text>
 					</View>
 					<View className='w-full h-max mb-6 justify-start'>
-						<View className='px-4 flex flex-row items-center gap-x-1.5 pb-10'>
-							<Image
-								source={Icons.Profile}
-								className='w-8 h-8'
-								tintColor='#666666'
-							/>
-							<Text className='text-lg font-GilroyMedium'>Account and Profile</Text>
-							<Image
-								source={Icons.Down}
-								tintColor='#666666'
-								className='w-8 h-8'
-							/>
-						</View>
-						<View className='px-4 flex flex-row items-center gap-x-1.5 pb-10'>
-							<Image
-								source={Icons.Settings}
-								className='w-8 h-8'
-								tintColor='#666666'
-							/>
-							<Text className='text-lg font-GilroyMedium'>Application preferences</Text>
-							<Image
-								source={Icons.Down}
-								tintColor='#666666'
-								className='w-8 h-8'
-							/>
-						</View>
-						<View className='px-4 flex flex-row items-center gap-x-1.5 pb-10'>
-							<Image
-								source={Icons.Padlock}
-								className='w-8 h-8'
-								tintColor='#666666'
-							/>
-							<Text className='text-lg font-GilroyMedium'>Security</Text>
-							<Image
-								source={Icons.Down}
-								tintColor='#666666'
-								className='w-8 h-8'
-							/>
-						</View>
-						<View className='px-4 flex flex-row items-center gap-x-1.5 pb-10'>
-							<Image
-								source={Icons.Accessible}
-								className='w-8 h-8'
-								tintColor='#666666'
-							/>
-							<Text className='text-lg font-GilroyMedium'>Accessibility</Text>
-							<Image
-								source={Icons.Down}
-								tintColor='#666666'
-								className='w-8 h-8'
-							/>
-						</View>
-						<View className='px-4 flex flex-row items-center gap-x-1.5 pb-10'>
-							<Image
-								source={Icons.LifeBuoy}
-								className='w-8 h-8'
-								tintColor='#666666'
-							/>
-							<Text className='text-lg font-GilroyMedium'>Help and support</Text>
-							<Image
-								source={Icons.Down}
-								tintColor='#666666'
-								className='w-8 h-8'
-							/>
-						</View>
-						<View className='px-4 flex flex-row items-center gap-x-1.5 pb-10'>
-							<Image
-								source={Icons.Info}
-								className='w-8 h-8'
-								tintColor='#666666'
-							/>
-							<Text className='text-lg font-GilroyMedium'>Legal information</Text>
-							<Image
-								source={Icons.Down}
-								tintColor='#666666'
-								className='w-8 h-8'
-							/>
-						</View>
+						{settings.map( ( setting, index ) => (
+							<View key={index} className='px-4 flex flex-row items-center gap-x-1.5 pb-10'>
+								<Image
+									source={setting.icon}
+									className='w-8 h-8'
+									tintColor='#666666'
+								/>
+								<Text className='text-lg font-GilroyMedium'>{setting.title}</Text>
+								<Image
+									source={Icons.Down}
+									tintColor='#666666'
+									className='w-8 h-8'
+								/>
+							</View>
+						) )}
+						<DefaultButton 
+							title='Log Out'
+							handlePress={() => router.replace( 'sign-in' )}
+							containerStyles={undefined}
+							textStyles='text-lg'
+						/>
 					</View>
-					<DefaultButton
-						title='Sign Out'
-						handlePress={() => router.replace( 'sign-in' )}
-						containerStyles='mt-2'
-					/>
 				</View>
 			</ScrollView>
 		</SafeAreaView>
