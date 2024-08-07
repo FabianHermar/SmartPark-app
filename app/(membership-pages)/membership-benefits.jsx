56bb2d1e4144a1bdea88d6c5418973f21bbd461f
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-06 18:45:57 -0600

    feat: Add MembershipBenefits component for membership benefits page

---
+++ app/(membership-pages)/membership-benefits.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const MembershipBenefits = () => {
+	return (
+		<View>
+			<Text>MembershipBenefits</Text>
+		</View>
+	)
+}
+
+export default MembershipBenefits
