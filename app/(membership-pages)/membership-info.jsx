e62b71721489dfeb6a9b0ec5e5cf086b6d12aa8a
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-06 18:46:02 -0600

    feat: Add MembershipInfo component for membership info page

---
+++ app/(membership-pages)/membership-info.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const MembershipInfo = () => {
+	return (
+		<View>
+			<Text>MembershipInfo</Text>
+		</View>
+	)
+}
+
+export default MembershipInfo
