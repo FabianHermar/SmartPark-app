b0358d81c54fea2c87414f193e5ff02476bb9991
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-08 13:24:07 -0600

    refactor: Update payment method page layout and components

--- app/(membership-pages)/payment-method.jsx
+++ app/(membership-pages)/payment-method.jsx
@@ -1,28 +1,19 @@
 import DefaultButton from '@/components/DefaultButton'
-import FormField from '@/components/FormField'
 import Pill from '@/components/Pill'
+import { Payments } from '@/constants'
+import { Image } from 'expo-image'
 import { LinearGradient } from 'expo-linear-gradient'
 import { router } from 'expo-router'
-import { useState } from 'react'
 import { ScrollView, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const PaymentMethod = () => {
-
-	const [ form, setForm ] = useState( {
-		name: '',
-		lastName: '',
-		telephone: '',
-		email: '',
-		password: ''
-	} )
-
 	return (
 		<SafeAreaView className='bg-white h-full'>
 			<LinearGradient
-				colors={[ '#000', '#0F4F75' ]}
-				start={[ 0, 0 ]}
-				end={[ 1, 0 ]}
+				colors={['#000', '#0F4F75']}
+				start={[0, 0]}
+				end={[1, 0]}
 				className='absolute top-0 left-0 w-full h-[850px] right-0'
 			/>
 			<ScrollView>
@@ -37,47 +28,95 @@ const PaymentMethod = () => {
 						/>
 					</View>
 					<View className='py-4'>
-						<Text className='text-white font-GilroyBold text-4xl'>Choose Payment Method</Text>
-						<Text className='text-neutral-400 font-GilroyRegular pt-2 text-base'>To complete your SmartPark + membership, please select your preferred payment method.</Text>
+						<Text className='text-white font-GilroyBold text-4xl'>
+							Choose Payment Method
+						</Text>
+						<Text className='text-neutral-400 font-GilroyRegular pt-2 text-base'>
+							To complete your SmartPark + membership, please select your
+							preferred payment method.
+						</Text>
 					</View>
 					<View>
-						<FormField
-							title='Name'
-							placeholder='John'
-							value={form.name}
-							handleChangeText={( e ) => setForm( { ...form, name: e } )}
-							textStyles='text-white'
-							otherStyles='my-3'
-						/>
-						<FormField
-							title='Last Name'
-							placeholder='Doe'
-							value={form.lastName}
-							handleChangeText={( e ) => setForm( { ...form, lastName: e } )}
-							textStyles='text-white'
-							otherStyles='my-3'
-						/>
-						<FormField
-							title='Telephone'
-							placeholder='222-555-5555'
-							value={form.telephone}
-							handleChangeText={( e ) => setForm( { ...form, telephone: e } )}
-							textStyles='text-white'
-							otherStyles='my-3'
-						/>
-						<FormField
-							title='Email'
-							placeholder='Enter your email'
-							value={form.email}
-							handleChangeText={( e ) => setForm( { ...form, email: e } )}
-							textStyles='text-white'
-							otherStyles='my-3'
-							keyboardType='email-address'
-						/>
+						<View className='py-4'>
+							<View className='flex flex-row items-center justify-between'>
+								<View className='flex flex-row items-center'>
+									<View className='w-20 h-14 bg-white rounded-xl flex items-center justify-center'>
+										<Image
+											source={Payments.Visa}
+											className='w-12 h-10'
+											contentFit='contain'
+										/>
+									</View>
+									<Text className='text-white font-GilroyBold text-lg pl-4'>
+										Visa
+									</Text>
+								</View>
+								<View className='w-6 h-6 bg-white rounded-full flex items-center justify-center'>
+									<Image source={undefined} className='w-4 h-4' />
+								</View>
+							</View>
+						</View>
+						<View className='py-4'>
+							<View className='flex flex-row items-center justify-between'>
+								<View className='flex flex-row items-center'>
+									<View className='w-20 h-14 bg-white rounded-xl flex items-center justify-center'>
+										<Image
+											source={Payments.MasterCard}
+											className='w-12 h-10'
+											contentFit='contain'
+										/>
+									</View>
+									<Text className='text-white font-GilroyBold text-lg pl-4'>
+										MasterCard
+									</Text>
+								</View>
+								<View className='w-6 h-6 bg-white rounded-full flex items-center justify-center'>
+									<Image source={undefined} className='w-4 h-4' />
+								</View>
+							</View>
+						</View>
+						<View className='py-4'>
+							<View className='flex flex-row items-center justify-between'>
+								<View className='flex flex-row items-center'>
+									<View className='w-20 h-14 bg-white rounded-xl flex items-center justify-center'>
+										<Image
+											source={Payments.Stripe}
+											className='w-12 h-8'
+											contentFit='contain'
+										/>
+									</View>
+									<Text className='text-white font-GilroyBold text-lg pl-4'>
+										Stripe
+									</Text>
+								</View>
+								<View className='w-6 h-6 bg-white rounded-full flex items-center justify-center'>
+									<Image source={undefined} className='w-4 h-4' />
+								</View>
+							</View>
+						</View>
+						<View className='py-4'>
+							<View className='flex flex-row items-center justify-between'>
+								<View className='flex flex-row items-center'>
+									<View className='w-20 h-14 bg-white rounded-xl flex items-center justify-center'>
+										<Image
+											source={Payments.PayPal}
+											className='w-12 h-8'
+											contentFit='contain'
+										/>
+									</View>
+									<Text className='text-white font-GilroyBold text-lg pl-4'>
+										PayPal
+									</Text>
+								</View>
+								<View className='w-6 h-6 bg-white rounded-full flex items-center justify-center'>
+									<Image source={undefined} className='w-4 h-4' />
+								</View>
+							</View>
+						</View>
 					</View>
 					<DefaultButton
 						title='Continue'
-						handlePress={() => router.push( 'vehicle-info' )}
+						handlePress={() => router.push('vehicle-info')}
 						containerStyles='mt-7'
 						isLoading={undefined}
 					/>
@@ -88,4 +127,3 @@ const PaymentMethod = () => {
 }
 
 export default PaymentMethod
-
