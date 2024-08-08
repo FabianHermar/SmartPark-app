6035bf5f182e7d04c5659fd7586b67423fa7e14b
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-08 13:24:26 -0600

    feat: Add payment methods constants

---
+++ constants/Payments.js
@@ -0,0 +1,11 @@
+import MasterCard from '@/assets/payment-methods/MasterCard.svg'
+import PayPal from '@/assets/payment-methods/Paypal.svg'
+import Stripe from '@/assets/payment-methods/Stripe.svg'
+import Visa from '@/assets/payment-methods/Visa.svg'
+
+export default {
+	MasterCard,
+	PayPal,
+	Stripe,
+	Visa,
+}
