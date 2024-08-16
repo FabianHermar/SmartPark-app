2e67f0ad7d84ca94f896fde7136c3bffbc61566a
Geo290 <jorgeemmanuel.2003@gmail.com>
2024-08-15 18:55:50 -0600

    Feat: retrive data from user session

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,64 +1,82 @@
-import AdditionalContent from "@/components/AdditionalContent";
-import CategoryCard from "@/components/CategoryCard";
-import { Icons, Images } from "@/constants";
-import { Image } from 'expo-image';
-import { router } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { ScrollView, Text, TouchableOpacity, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import AdditionalContent from '@/components/AdditionalContent'
+import CategoryCard from '@/components/CategoryCard'
+import { Icons, Images } from '@/constants'
+import { Image } from 'expo-image'
+import { router } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import { useEffect, useState } from 'react'
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { jwtDecode } from 'jwt-decode'
 
-export default function UserIndex () {
-	const [ selectedCheckbox, setSelectedCheckbox ] = useState( null )
-	const [ isVisible, setIsVisible ] = useState( false )
+export default function UserIndex() {
+	const [selectedCheckbox, setSelectedCheckbox] = useState(null)
+	const [isVisible, setIsVisible] = useState(false)
+	const [user, setUser] = useState(null);
+	const [token, setToken] = useState(null);
+
+	useEffect(()=>{
+		const fetchToken = async () => {
+			try {
+				const token = await AsyncStorage.getItem('token')
+				const decoded = jwtDecode(token)
+				const userName = decoded.names;
+				setToken(token);
+				setUser(userName);
+
+			} catch (error) {
+				console.log('Error retrieving token '+error.message);
+			}
+		};
+
+		fetchToken();
+	}, [])
 
 	const checkboxes = [
 		{ id: 1, text: 'Car', image: Icons.Car },
 		{ id: 2, text: 'Bike', image: Icons.Motorcycle },
 		{ id: 3, text: 'Bicycle', image: Icons.Bicycle },
-		{ id: 4, text: 'Truck', image: Icons.Truck },
-	];
+		{ id: 4, text: 'Truck', image: Icons.Truck }
+	]
 
-	const handlePress = ( id ) => {
-		if ( selectedCheckbox === id ) {
-			setIsVisible( false )
-			setTimeout( () => setSelectedCheckbox( null ), 500 )
+	const handlePress = (id) => {
+		if (selectedCheckbox === id) {
+			setIsVisible(false)
+			setTimeout(() => setSelectedCheckbox(null), 500)
 		} else {
-			setSelectedCheckbox( id )
-			setIsVisible( true )
+			setSelectedCheckbox(id)
+			setIsVisible(true)
 		}
-	};
+	}
 
 	return (
-		<SafeAreaView className='bg-white h-full' edges={{ top: 'maximum', left: 'maximum', right: 'maximum' }}>
+		<SafeAreaView
+			className='bg-white h-full'
+			edges={{ top: 'maximum', left: 'maximum', right: 'maximum' }}
+		>
 			<ScrollView contentContainerStyle={{ height: '100%' }}>
 				<View className='bg-black w-full h-[300px]'>
 					<View className='py-8 flex flex-row justify-center items-center gap-x-32'>
 						<TouchableOpacity
-							onPress={() => router.push( 'profile' )}
+							onPress={() => router.push('profile')}
 							activeOpacity={0.7}
 							className={`bg-pill/80 backdrop-blur px-4 py-1 w-44 justify-center rounded-full items-center`}
 						>
 							<View className='flex flex-row mx-4 items-center justify-center'>
-								<Image
-									source={Images.UserPill}
-									className='w-12 h-12'
-								/>
+								<Image source={Images.UserPill} className='w-12 h-12' />
 								<View className='pl-2'>
 									<Text className='font-GilroyMedium text-[#C9C9C9]'>
 										Welcome back!
 									</Text>
 									<Text className='font-GilroyBold text-white text-lg'>
-										John Doe
+										{user}
 									</Text>
 								</View>
 							</View>
 						</TouchableOpacity>
 						<View className='bg-[#3C3F46] w-10 h-10 rounded-lg flex items-center justify-center'>
-							<Image
-								source={Icons.GrayBell}
-								className='w-6 h-6'
-							/>
+							<Image source={Icons.GrayBell} className='w-6 h-6' />
 							<View className='absolute w-2 h-2 bg-red-600 rounded-full top-2.5 right-2.5' />
 						</View>
 					</View>
@@ -71,7 +89,9 @@ export default function UserIndex () {
 								source={Icons.Search}
 								className='w-6 h-6 absolute top-3 right-3'
 							/>
-							<Text className='font-GilroyRegular text-secondary text-base absolute top-3 left-3'>Find parking area...</Text>
+							<Text className='font-GilroyRegular text-secondary text-base absolute top-3 left-3'>
+								Find parking area...
+							</Text>
 						</View>
 					</View>
 				</View>
@@ -80,16 +100,19 @@ export default function UserIndex () {
 					<Text className='text-2xl font-GilroySemibold'>Category</Text>
 					<View>
 						<View className='flex flex-row justify-center items-center'>
-							{checkboxes.map( ( checkbox ) => (
+							{checkboxes.map((checkbox) => (
 								<CategoryCard
 									key={checkbox.id}
 									text={checkbox.text}
 									imageSource={checkbox.image}
 									checked={selectedCheckbox === checkbox.id}
-									onPress={() => handlePress( checkbox.id )}
-									disabled={selectedCheckbox !== null && selectedCheckbox !== checkbox.id}
+									onPress={() => handlePress(checkbox.id)}
+									disabled={
+										selectedCheckbox !== null &&
+										selectedCheckbox !== checkbox.id
+									}
 								/>
-							) )}
+							))}
 						</View>
 					</View>
 				</View>
@@ -97,14 +120,14 @@ export default function UserIndex () {
 					<AdditionalContent
 						isVisible={isVisible}
 						onAnimationEnd={() => {
-							if ( !isVisible ) {
-								setSelectedCheckbox( null )
+							if (!isVisible) {
+								setSelectedCheckbox(null)
 							}
 						}}
 					/>
 				)}
 			</ScrollView>
-			<StatusBar backgroundColor="light" style='dark' />
+			<StatusBar backgroundColor='light' style='dark' />
 		</SafeAreaView>
 	)
 }
