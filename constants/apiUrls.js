73265836fd1eae2c302cd21721786a686c4671d1
Geo290 <jorgeemmanuel.2003@gmail.com>
2024-08-15 18:58:28 -0600

    Fix: set right backend url

--- constants/apiUrls.js
+++ constants/apiUrls.js
@@ -1,2 +1,2 @@
-export const USER_BASE_URL = 'https://parkease-api.onrender.com/api/v1/user/';
-export const MEMBERSHIP_BASE_URL = 'https://parkease-api.onrender.com/api/v1/membership/';
+export const USER_BASE_URL = 'https://parkease-backend.onrender.com/api/v1/user';
+export const MEMBERSHIP_BASE_URL = 'https://parkease-backend.onrender.com/api/v1/membership';
