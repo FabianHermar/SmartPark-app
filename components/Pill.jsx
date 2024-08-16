56df7d0d36393ddddd873818ec2a6ba20d3729a8
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-16 02:20:56 -0600

    feat: Refactor Pill component to use Expo BlurView for background

--- components/Pill.jsx
+++ components/Pill.jsx
@@ -1,24 +1,41 @@
 import { Icons } from '@/constants'
+import { BlurView } from 'expo-blur'
 import { Image } from 'expo-image'
 import { Text, TouchableOpacity, View } from 'react-native'
 
-const Pill = ( { title, handlePress, containerStyles, textStyles, isLoading, iconColor } ) => {
-  return (
-    <TouchableOpacity
-      onPress={handlePress}
-      activeOpacity={0.7}
-			className={`bg-black/10 backdrop-blur px-4 py-2 w-20 justify-center rounded-full items-center ${containerStyles} ${isLoading ? 'opacity-50' : ''}`}
-    >
-      <View className='flex flex-row gap-0.5 items-center justify-center'>
-        <Image
-          source={Icons.Left}
-          className='w-4 h-4 text-secondary'
-					tintColor={iconColor}
-        />
-        <Text className={`text-secondary font-GilroySemibold text-xs ${textStyles}`}>{title}</Text>
-      </View>
-    </TouchableOpacity>
-  )
+const Pill = ({
+	title,
+	handlePress,
+	containerStyles,
+	textStyles,
+	iconColor
+}) => {
+	return (
+		<TouchableOpacity
+			onPress={handlePress}
+			activeOpacity={0.7}
+			className={`px-4 py-2 justify-center rounded-full items-center ${containerStyles}`}
+		>
+			<BlurView
+				style={{ borderRadius: 20, overflow: 'hidden' }}
+				className='bg-[#111111]/5 w-16 px-4 py-2 items-center justify-center'
+				intensity={20}
+			>
+				<View className='flex flex-row gap-0.5 items-center justify-center'>
+					<Image
+						source={Icons.Left}
+						className='w-4 h-4 text-secondary'
+						tintColor={iconColor}
+					/>
+					<Text
+						className={`text-secondary font-GilroySemibold text-xs ${textStyles}`}
+					>
+						{title}
+					</Text>
+				</View>
+			</BlurView>
+		</TouchableOpacity>
+	)
 }
 
 export default Pill
