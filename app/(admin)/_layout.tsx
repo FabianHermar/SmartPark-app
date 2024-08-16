3df868e419ba0d652eadba7c9fed1a53d926f355
Geo290 <jorgeemmanuel.2003@gmail.com>
2024-08-16 09:55:37 -0600

    Fix: former routes fixed; new Settings Screen route added

--- app/(admin)/_layout.tsx
+++ app/(admin)/_layout.tsx
@@ -5,32 +5,15 @@ const AdminLayout = () => {
 	return (
 		<>
 			<Stack>
-				<Stack.Screen
-					name='admin'
-					options={{ headerShown: false }}
-				/>
-				<Stack.Screen
-					name='clients'
-					options={{ headerShown: false }}
-				/>
-				<Stack.Screen
-					name='members'
-					options={{ headerShown: false }}
-				/>
-				<Stack.Screen
-					name='profiles'
-					options={{ headerShown: false }}
-				/>
-				<Stack.Screen
-					name='register'
-					options={{ headerShown: false }}
-				/>
-				<Stack.Screen
-					name='start'
-					options={{ headerShown: false }}
-				/>
+				<Stack.Screen name='Admin' options={{ headerShown: false }} />
+				<Stack.Screen name='Clients' options={{ headerShown: false }} />
+				<Stack.Screen name='members' options={{ headerShown: false }} />
+				<Stack.Screen name='profiles' options={{ headerShown: false }} />
+				<Stack.Screen name='Register' options={{ headerShown: false }} />
+				<Stack.Screen name='Start' options={{ headerShown: false }} />
+				<Stack.Screen name='admin-settings' options={{ headerShown: false }} />
 			</Stack>
-			<StatusBar backgroundColor="white" style='dark' />
+			<StatusBar backgroundColor='white' style='dark' />
 		</>
 	)
 }
